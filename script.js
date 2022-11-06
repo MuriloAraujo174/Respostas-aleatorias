dcadd96297acaa9d306206e65306e669ac324956
antonio murilo <107326926+MuriloAraujo174@users.noreply.github.com>
2022-11-06 19:23:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim."
+]
+
+
+function fazerPergunta() {
+
+  if (inputPergunta.value == "") {
+    alert("Digite sua pergunta")
+    return
+  }
+
+  buttonPerguntar.setAttribute("disabled", true)
+
+  const pergunta = "<div>" + inputPergunta.value + "<div>"
+
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  elementoResposta.style.opacity = 1;
+
+  setTimeout(function() {
+    elementoResposta.style.opacity = 0;
+    buttonPerguntar.removeAttribute('disabled')
+  }, 3000)
+}
